fbba16de4d4014db0b0f4920bf44c46b30ef894b
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2022-11-29 18:59:15 -0300

    Finalizando projeto adicionando som

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 import Controls from "./controls.js"
 import Timer from "./timer.js"
-
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
-let minutes = Number(minutesDisplay.textContent)
-let timerTimeOut
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay,
+} from "./elements.js"
 
 const controls = Controls({
   buttonPause,
@@ -22,43 +21,9 @@ const controls = Controls({
 const timer = Timer({
   minutesDisplay, 
   secondsDisplay, 
-  timerTimeOut, 
   resetControls: controls.reset
 })
 
-buttonPlay.addEventListener('click', function() {
-  controls.play()
-  timer.countdown()
-})
+const sound = Sound()
 
-buttonPause.addEventListener('click', function() {
-  controls.pause()
-  clearTimeout(timerTimeOut)
-})
-
-buttonStop.addEventListener('click', function() {
-  controls.reset()
-  timer.reset()
-})
-
-buttonSoundOff.addEventListener('click', function() {
-  buttonSoundOn.classList.remove('hide')
-  buttonSoundOff.classList.add('hide')
-})
-
-buttonSoundOn.addEventListener('click', function() {
-  buttonSoundOn.classList.add('hide')
-  buttonSoundOff.classList.remove('hide')
-})
-
-buttonSet.addEventListener('click', function() {
-  let newMinutes = controls.getMinutes()
-
-  if (!newMinutes) {
-    timer.reset()
-    return
-  }
-
-  minutes = newMinutes
-  timer.updateDisplay(minutes, 0)
-})
+Events({controls, timer, sound})
